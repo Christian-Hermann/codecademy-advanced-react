fef56a74a506e8a2c5893480d978ca0a93c13aec
Your NameChristian <christianhermanndev@gmail.com>
2026-01-06 12:09:35 -0600

    Add ternanry for toggle switch

--- Advanced_React/Review /Theme Toggler/hooks/useTheme.js
+++ Advanced_React/Review /Theme Toggler/hooks/useTheme.js
@@ -2,4 +2,10 @@ import { useState, useEffect } from "react";
 
 const useTheme = () => {
   const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const onToggleTheme = () => {};
 };
